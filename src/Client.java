08e471ed1dde1fb4bcdb53a6f279b7ca3542e30e
samarth36483 <raipuriyasamarth@gmail.com>
2023-09-03 12:31:45 +0530

    correction in client class

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,13 @@ public class Client {
 
 	public static void main(String[] args) {
 		Customer customer = new CustomerBuilder()
-				.firstName("Sam");
+				.firstName("Samarth")
+				.lastName("Raipuriya")
+				.primaryEmail("abc@123")
+				.primaryMobileNumber("1234567890")
+				.build();
+		
+		System.out.println(customer.toString());
 
 	}
 
